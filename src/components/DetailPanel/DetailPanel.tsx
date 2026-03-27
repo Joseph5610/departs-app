b116c0df34fd24a47e3c87c1a6b2921564cadeba
Jozef Hudacek <jozo5610@gmail.com>
2026-03-27 11:44:02 +0100

    fix: adjust DetailPanel scrolling behavior and bump package version.

--- src/components/DetailPanel/DetailPanel.tsx
+++ src/components/DetailPanel/DetailPanel.tsx
@@ -146,9 +146,9 @@ export const DetailPanel: React.FC<DetailPanelProps> = React.memo(({ isOpen, onC
                     */}
                     <div 
                         data-vaul-no-drag
-                        className="flex-1 overflow-y-auto px-6 pointer-events-auto overscroll-contain custom-scrollbar"
+                        className="flex-1 min-h-0 overflow-y-auto px-6 pointer-events-auto overscroll-contain custom-scrollbar"
                     >
-                        <div className="pb-[50dvh]">
+                        <div className="pb-[40dvh]">
                             {children}
                         </div>
                     </div>
